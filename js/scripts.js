581446ea0f03c9cd8e5b2e56867d4d07775f7488
Iskren Alexandrov <iskren.alexandrov@gmail.com>
2021-03-28 22:36:06 +0300

    small changes3

--- js/scripts.js
+++ js/scripts.js
@@ -92,7 +92,7 @@ function onSubmit(){
 
     // примерен вход: Юнак без , рана не може
     // req
-    $.postCORS("http://0.0.0.0:5000/",{ body : input },function(response){
+    $.postCORS("http://15.237.27.239/",{ body : input },function(response){
         console.log(response);
         $.showBtn();
         // има грешка в input-а
